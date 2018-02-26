f3b9753843cc78d228c0bb95220ce1b4454fb8bd
MaxiStarovoitov <rackezik@gmail.com>
2018-02-26 07:27:14 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+	// your implementation here
+	if (typeof temperature == 'number') {
+		const cTemp = temperature;
+		const fTemp = cTemp * 9/5 + 32;
+
+		return fTemp;
+	}
+
+	return false;
 };
